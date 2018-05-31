7ee6519d7492cc9765ce6d95c30f89268ca52d80
YOYOGAMES\sam.cumming <samuel.cumming@yoyogames.com>
2018-05-31 13:29:02 +0100

    Transport commit

--- Planetariarm/app/src/main/cpp/native-lib.cpp
+++ Planetariarm/app/src/main/cpp/native-lib.cpp
@@ -11,3 +11,19 @@ Java_com_forsamori_planetariarm_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
+
+/* JNIEXPORT datatype
+ * JNICALL Java_domain_Context_functionName
+ * () <- Need to pass in the JNIEnvironment and the returning object
+ * Remember to set extern "C" before functions*/
+extern "C"
+JNIEXPORT jstring
+
+JNICALL
+Java_com_forsamori_planetariarm_MainActivity_customHelloWorld(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string myHello = "Yo, its'chu boy, skinny native!";
+    return env->NewStringUTF(myHello.c_str());
+}
--- Planetariarm/app/src/main/java/com/forsamori/planetariarm/MainActivity.java
+++ Planetariarm/app/src/main/java/com/forsamori/planetariarm/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -18,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //tv.setText(stringFromJNI());
+        tv.setText(customHelloWorld());
     }
 
     /**
@@ -26,4 +28,5 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native String customHelloWorld();
 }
